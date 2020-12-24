af1dfd37fc7f64666f47ee7c4d577d9824e1744b
danhngocphh <64898520+danhngocphh@users.noreply.github.com>
2020-12-24 09:14:19 +0700

    settimezone

--- db.js
+++ db.js
@@ -553,17 +553,19 @@ function HtmlEncode(s)
 
 // Use of Date.now() method 
 function getDateTime(dateIP){
-    let today = new Date(dateIP.toLocaleString('en-US', {
+    let today = new Date(dateIP);
+    today.toLocaleString('en-US', {
         timeZone: 'Asia/Bangkok'
-      }));
+      })
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     return time;
   }
 
   function getDateT(dateIP){
-    let today = new Date(dateIP.toLocaleString('en-US', {
+    let today = new Date(dateIP);
+    today.toLocaleString('en-US', {
         timeZone: 'Asia/Bangkok'
-      }));
+      })
     let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
     return date;
   }  
